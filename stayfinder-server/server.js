d064233947b80e2da722ae93486ecd667ca17bd2
DivyaMeghanaMamidipalli <mamidipalli.1@iitj.ac.in>
2025-06-20 17:48:53 +0530

    Cors Error Resolution

--- stayfinder-server/server.js
+++ stayfinder-server/server.js
@@ -17,16 +17,13 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://stay-finder-app-xi.vercel.app/'], 
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
