1edeb6f545eed018ad55d2131f034c536c1f8fd6
DivyaMeghanaMamidipalli <mamidipalli.1@iitj.ac.in>
2025-06-20 18:02:28 +0530

    cors issue addressed again

--- stayfinder-server/server.js
+++ stayfinder-server/server.js
@@ -10,13 +10,16 @@ require('dotenv').config();
 
 const app = express();
 
+
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://stay-finder-app-xi.vercel.app/'], 
+  origin: ['http://localhost:5173', 'https://stay-finder-app-xi.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+
+
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
